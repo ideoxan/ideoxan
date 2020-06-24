4c2368ae83e1f70faf30ed3248cbf7f7a9d5761b
Skyclo <hello.skyclo@gmail.com>
2020-06-24 12:47:30 -0400

    Dynamic Lesson Guides/Loading and Commenting Run

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const fs = require('fs')
 const app = express()
 app.set('view engine', 'ejs')
 app.use('/static', express.static('static')) // CSS, JS, HTML (NOT EJS), images, etc.
-
+app.use(express.urlencoded({ extended: true }))
 
 /* ---------------------------------------------------------------------------------------------- */
 /*                                            CONSTANTS                                           */
@@ -26,12 +26,39 @@ app.get('/', async (req, res) => {
     res.render('index')
 })
 
+app.get('/editor/:course/:lesson', async (req, res) => {
+    res.render('index', {
+        ServerAppData: { //here for a reason just leave it alone :^)
+            ideoxan: {
+                lessonData: {
+                    course: req.params.course,
+                    lesson: req.params.lesson,
+                    meta: JSON.stringify(await readIXConfig(`../cur/curriculum-${req.params.course}/.ideoxan`))
+                }
+            }
+        }
+    })
+})
+
 app.get('/ping', async (req, res) => {
     res.status(200)
     res.end('All Good :)')
 })
 
 app.listen(process.env.PORT || port)
+
+
 /* ---------------------------------------------------------------------------------------------- */
 /*                                             METHODS                                            */
 /* ---------------------------------------------------------------------------------------------- */
+
+/**
+ * Reads a .ideoxan configuration file in a course directory and returns a JSON object
+ * 
+ * @param {String} path A valid path to a course directory
+ * @returns {Promise<JSON>} A JSON object of course metadata/configuration
+ */
+async function readIXConfig(path) {
+    let data = await fs.promises.readFile(require.resolve(path))
+    return (data)? JSON.parse(data) : null
+}
--- static/js/TabManager/Tab.js
+++ static/js/TabManager/Tab.js
@@ -7,8 +7,14 @@ define([
     'TabManager/tabElement'
 ], (ace, JSMode, CSSMode, HTMLMode, EditSession, tabElement) => {
     return class Tab {
-        constructor(language, filename, targetID, id, icon) {
-            this.language = language
+        /**
+         * Creates a new tab object and the corresponding DOM Element
+         * @param {String} filename - The name of the tab
+         * @param {String} targetID - The Element ID to append the tab to
+         * @param {String} id - The desired ID of the new tab
+         * @param {String} [icon=] 
+         */
+        constructor(filename, targetID, id, icon) {
             this.filename = filename
 
             tabElement.createTabElement('#' + targetID, id)
@@ -20,37 +26,72 @@ define([
                 'css3': CSSMode.Mode,
                 'javascript': JSMode.Mode,
             }
-            if (this.language != null) {
+
+            this.ext = filename.split('.').pop() // Gets file extension
+            this.language = ''
+            
+            if (this.ext != this.filename) { // If there is no dot symbol in the name it will return the same as the filename
+                switch (this.ext) { // What file extension
+                    case 'html': this.language = 'html5'; break;
+                    case 'js': this.language = 'javascript'; break;
+                    case 'css': this.language = 'css3'; break;
+                    default: this.language = null; break;
+                }
+
                 this.session = new EditSession.EditSession('', new this.modes[this.language]())
                 this.id.getElementsByClassName('ico-12px')[0].classList = `mdi mdi-language-${this.language} ico-12px ico-${this.language}`
             } else {
                 this.id.getElementsByClassName('ico-12px')[0].classList = `mdi mdi-${icon} ico-12px ico-white`
+                this.language = null
             }
             this.id.getElementsByClassName('editor-tabs-filename')[0].innerHTML = filename
         }
 
+        /**
+         * Sets the Tab to the active class
+         */
         setActive() {
             this.id.classList.add('editor-tabs-t-active')
             this.id.classList.remove('editor-tabs-t-inactive')
         }
 
+        /**
+         * Sets the Tab to the inactive class
+         */
         setInactive() {
             this.id.classList.add('editor-tabs-t-inactive')
             this.id.classList.remove('editor-tabs-t-active')
         }
 
+        /**
+         * Sets the Ace Editor Session
+         * @param {EditSession.EditSession} session 
+         */
         setSession(session) {
             this.session = session
         }
 
+        /**
+         * Gets the Ace Editor Document Object associated with the Tab
+         * @return {EditSession.EditSession.Document}
+         */
         getDocument() {
             return this.session.getDocument()
         }
 
+        /**
+         * Gets the Ace Editor Session associated with the Tab
+         * @return {EditSession.EditSession}
+         */
         getSession() {
             return this.session
         }
 
+        /**
+         * Creates a new event listener that exectues the binded callback
+         * @param {Function} funct - A function to be executed when the Tab is clicked
+         * 
+         */
         onClick(funct) {
             this.id.addEventListener('click', funct)
         }

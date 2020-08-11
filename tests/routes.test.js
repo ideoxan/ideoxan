a7cbc2f37cf37cb368177c0ff01fbab48b187205
Skyclo <hello.skyclo@gmail.com>
2020-08-10 22:16:29 -0400

    Updated Tests

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -15,7 +15,21 @@ describe('(SERVER) Checking Server Response', () => {
     })
 })
 
+describe('(SERVER) Request Error Pages', () => {
+    it('Should receive 404 NOT FOUND HTTP response for GET /a-404-page', async (done) => {
+        const res = await request(app).get('/a-404-page')
+        expect(res.statusCode).toEqual(404)
+        done()
+    })
+})
+
 describe('(SERVER) Request Main Pages', () => {
+    it('Should receive 200 OK HTTP response for GET /', async (done) => {
+        const res = await request(app).get('/')
+        expect(res.statusCode).toEqual(200)
+        done()
+    })
+
     it('Should receive 200 OK HTTP response for GET /index', async (done) => {
         const res = await request(app).get('/index')
         expect(res.statusCode).toEqual(200)

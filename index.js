0b08b6e4a1f8bb2c016ec6c22f6572300e3c6c5a
Skyclo <hello.skyclo@gmail.com>
2020-10-15 21:21:59 -0400

    Added improved server launch display

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Load local .env config if not prod
 process.env.NODE_ENV != 'production'? require('dotenv').config():{}
 
+const c = require('chalk')                                      // Terminal coloring
+
 // Config
 try {
     global.cfg = require('./config.json')
@@ -11,5 +13,16 @@ try {
 const app = require('./src/apps/main').app                      // Creates Ideoxan Server
 
 app.listen(cfg.server.port||3080, ()=> {                        // Listens on environment set port
-    console.log(`${cfg.server.name} Server Online`)
+    console.log(`\n\n`)
+    console.log(`${c.greenBright('⦿')}  ${c.bold.italic(cfg.server.name + ' Main Server')}`)
+    console.log(`\tStatus: ${c.greenBright('online')}`)
+    console.log(`\tNetwork:`)
+    console.log(`\t\tIP: ${c.keyword('orange')(cfg.server.ip||'localhost')}`)
+    console.log(`\t\tPort: ${c.keyword('orange')(cfg.server.port||3080)}`)
+    console.log(`\tPID: ${process.pid}`)
+    console.log(`\tLoaded Apps:`)
+    cfg.server.apps.forEach(_app => {
+        console.log(`\t\t${_app}: ${c.bgGreenBright(' ✔ OK ')}`)
+    })
+    console.log(`\tLogs:`)
 }) 

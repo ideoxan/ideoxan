aa0a678c1c0d78b79c5673d449899136b8faab87
Skyclo <hello.skyclo@gmail.com>
2020-10-17 12:34:34 -0400

    Abstracted port and ip to vars

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+const c = require('chalk')                                      // Terminal coloring
+
 // Load local .env config if not prod
 process.env.NODE_ENV != 'production'? require('dotenv').config():{}
-
-const c = require('chalk')                                      // Terminal coloring
 
 // Config
 try {
@@ -10,17 +10,20 @@ try {
     global.cfg = require('./config.default.json')
 }
 
+const port = process.env.PORT || cfg.server.port || 3080
+const ip = cfg.server.ip || 'localhost'
+
 const app = require('./src/apps/main').app                      // Creates Ideoxan Server
 
-app.listen(cfg.server.port||process.env.PORT||3080, ()=> {      // Listens on environment set port
+app.listen(port, ()=> {      // Listens on environment set port
     console.log(`\n\n`)
     console.log(`${c.greenBright('⦿')}  ${c.bold.italic(cfg.server.name + ' Main Server')}`)
     if (process.env.NODE_ENV != 'production')
-        console.log(c.magentaBright.italic(`\t(DEV) Now you can visit the website in testing mode at http://${cfg.server.ip||'localhost'}:${cfg.server.port||3080}/`))
+        console.log(c.magentaBright.italic(`\t(DEV) Now you can visit the website in testing mode at http://${ip}:${port}/`))
     console.log(`\tStatus: ${c.greenBright('online')}`)
     console.log(`\tNetwork:`)
-    console.log(`\t\tIP: ${c.keyword('orange')(cfg.server.ip||'localhost')}`)
-    console.log(`\t\tPort: ${c.keyword('orange')(cfg.server.port||3080)}`)
+    console.log(`\t\tIP: ${c.keyword('orange')(ip)}`)
+    console.log(`\t\tPort: ${c.keyword('orange')(port)}`)
     console.log(`\tPID: ${process.pid}`)
     console.log(`\tLoaded Apps:`)
     cfg.server.apps.forEach(_app => {

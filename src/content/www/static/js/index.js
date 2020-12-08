e6c07409020c5960dab71da43d07910097382a4d
Skyclo <hello.skyclo@gmail.com>
2020-12-07 19:13:30 -0500

    Added dropdown functionality

--- src/content/www/static/js/index.js
+++ src/content/www/static/js/index.js
@@ -1,18 +1,31 @@
 window.onload = () => {
-    console.log( 'Hi' )
+    /* ---------------------------------------------------------------------------------------------- */
+    /*                                           NAVIGATION                                           */
+    /* ---------------------------------------------------------------------------------------------- */
     /* -------------------------------------- Navigation Links -------------------------------------- */
     const navLinks = document.getElementsByClassName( 'nav-link' )
     for ( let i = 0; i < navLinks.length - 1; i++ ) {
-        const dropdown = document.querySelectorAll( '#nav-links-container .nav-dropdown' ) || null
-        console.log( dropdown )
-        if ( dropdown[ i ] ) {
+        const navDropdown = document.querySelectorAll( '#nav-links-container .nav-dropdown' ) || []
+        if ( navDropdown[ i ] ) {
             navLinks[ i ].addEventListener( 'mouseover', () => {
-                dropdown[ i ].classList.remove( 'hidden' )
+                navDropdown[ i ].classList.remove( 'hidden' )
             } )
             navLinks[ i ].addEventListener( 'mouseout', () => {
-                dropdown[ i ].classList.add( 'hidden' )
+                navDropdown[ i ].classList.add( 'hidden' )
             } )
         }
 
+    }
+
+    /* ---------------------------------------- Account Links --------------------------------------- */
+    const accountButton = document.getElementById('nav-account-container')
+    const accountDropdown = document.querySelector('#nav-account-container .nav-account-dropdown') || null
+    if (accountDropdown) {
+        accountButton.addEventListener( 'mouseover', () => {
+            accountDropdown.classList.remove( 'hidden' )
+        } )
+        accountButton.addEventListener( 'mouseout', () => {
+            accountDropdown.classList.add( 'hidden' )
+        } )
     }
 }

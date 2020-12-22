a140d1a2246471713eafd828cf77f75c52fe7937
Skyclo <hello.skyclo@gmail.com>
2020-12-22 13:35:19 -0500

    Fixed color contrast between navigation and body background

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,13 @@ module.exports = {
     fontFamily: {
       'sans': ['Gilroy','ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe\\ UI"', 'Roboto', '"Helvetica\\ Neue"', 'Arial', '"Noto\\ Sans"', 'sans-serif', '"Apple\\ Color\\ Emoji"', '"Segoe\\ UI\\ Emoji"', '"Segoe\\ UI\\ Symbol"', '"Noto\\ Color\\ Emoji"']
     },
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          '950': '#070b14'
+        }
+      }
+    },
   },
   variants: {
     extend: {},

fa91b530ff53bf9498b3b7a1d44331a39fd02fa7
Skyclo <hello.skyclo@gmail.com>
2021-01-30 20:29:29 -0500

    🗃 feat(db, server): add User schema

---
+++ models/User.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+const { v4: uuidv4 } = require('uuid')
+
+const UserSchema = new mongoose.Schema({
+    uid: {
+        type: String,
+        required: true,
+        default: uuidv4()
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    roles: {
+        type: Array,
+        required: true,
+        default: [0]
+    },
+    created: {
+        type: Date,
+        required: true,
+        default: Date.now().toString(),
+    },
+    lastLogin: {
+        type: Date,
+        required: true,
+        default: Date.now().toString(),
+    },
+    loginStreak: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    bio: {
+        type: String,
+        maxlength: 250,
+        required: false,
+        default: 'Hello, World! I am a new Ideoxan user!'
+    }
+})
+
+module.exports = mongoose.model('users', UserSchema)

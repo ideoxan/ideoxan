25184b5d412247aaed4f188985c560c5fd29968e
Skyclo <hello.skyclo@gmail.com>
2021-01-30 20:47:07 -0500

    🛂 feat(server): add routes GET /api/user/auth and GET /api/user/create

---
+++ routes/api/userCreate.js
@@ -0,0 +1,36 @@
+/* ---------------------------------------------------------------------------------------------- */
+/*                                             MODULES                                            */
+/* ---------------------------------------------------------------------------------------------- */
+/* ------------------------------------------- Models ------------------------------------------- */
+const Users                     = require(serverConfig.paths.models + '/User')
+/* --------------------------------------- Authentication --------------------------------------- */
+const passport                  = require('passport')
+const bcrypt                    = require('bcryptjs')
+
+
+/* ---------------------------------------------------------------------------------------------- */
+/*                                         INITIALIZATION                                         */
+/* ---------------------------------------------------------------------------------------------- */
+/* ------------------------------------------ Endpoint ------------------------------------------ */
+exports.route = 'user/create/'
+
+
+/* ---------------------------------------------------------------------------------------------- */
+/*                                           CONTROLLER                                           */
+/* ---------------------------------------------------------------------------------------------- */
+exports.post = (req, res, next) => {
+    try {
+        bcrypt.hash(req.body.password, Number.parseInt(process.env.PWD_HASH), async (err, pwdHash) => {
+            if (err) next(err)
+            await Users.create({
+                email: req.body.email.toLowerCase(),
+                username: req.body.displayName.toLowerCase(),
+                name: req.body.displayName.toLowerCase(),
+                password: pwdHash
+            })
+        })
+        res.redirect('/login')
+    } catch (err) {
+        next(err)
+    }
+}

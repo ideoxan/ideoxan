b4c280cf7842eb2c531230109e80809d3d6788d8
Skyclo <hello.skyclo@gmail.com>
2021-05-23 13:26:42 -0400

    ✨ feat: add user termination request api endpoint (POST /api/user/terminate)

---
+++ routes/api/userTerminate.js
@@ -0,0 +1,47 @@
+/* ---------------------------------------------------------------------------------------------- */
+/*                                             MODULES                                            */
+/* ---------------------------------------------------------------------------------------------- */
+/* ------------------------------------------- Models ------------------------------------------- */
+const Users                     = require(serverConfig.paths.models + '/User')
+/* ------------------------------------------ Utilities ----------------------------------------- */
+// HTTP Error Codes
+const HTTPError                 = require(serverConfig.paths.utilities + '/HTTPError')
+
+
+
+/* ---------------------------------------------------------------------------------------------- */
+/*                                         INITIALIZATION                                         */
+/* ---------------------------------------------------------------------------------------------- */
+/* ------------------------------------------ Endpoint ------------------------------------------ */
+exports.route = 'user/terminate'
+
+
+/* ---------------------------------------------------------------------------------------------- */
+/*                                           CONTROLLER                                           */
+/* ---------------------------------------------------------------------------------------------- */
+exports.post = async (req, res, next) => {
+    try {
+        if (req.isAuthenticated()) {
+            let user = await Users.findOne({uid: req.session.passport.user})|| null
+            if (!user) return next()
+
+            let userObject = user.toObject()
+
+            if (userObject.roles.includes(40899)) {
+                let serverError = new HTTPError(req, res, HTTPError.constants.HTTP_ERROR_CODES['403'])
+                return serverError.render()
+            }
+
+            await user.deleteOne()
+            req.logOut()
+            if (req.session) req.session.destroy()
+            if (req.accepts('html')) {
+                res.redirect('/')
+            }
+        } else {
+            next()
+        }
+    } catch (err) {
+        next(err)
+    }
+}

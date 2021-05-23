76a71e8a1c35ac9fae3b307153c18cffccbe273a
Skyclo <hello.skyclo@gmail.com>
2021-05-23 12:49:15 -0400

    ✨ feat: add login streaks

--- middlewares/authHandler.js
+++ middlewares/authHandler.js
@@ -5,10 +5,31 @@ module.exports = (passport) => {
     passport.use('local', localAuth)
 
     passport.serializeUser((user, done) => {
-        done(null, user.uid)
+        // Fun fact ^ ... the user object is an acutal db object
+
+        // User streaks
+        let lastLogin = new Date(user.lastLogin) // Gets the last login date
+        let currentLogin = new Date() // Gets the current login date
+
+        // Computes deltas
+        let deltaDays = currentLogin.getDate() - lastLogin.getDate() // Delta days
+        let deltaMonths = currentLogin.getMonth() - lastLogin.getMonth() // Delta months
+        let deltaYears = currentLogin.getFullYear() - lastLogin.getFullYear() // Delta years
+
+        // If there is no change in months and years, and days diff is equal to 1...
+        if (deltaMonths == 0 && deltaYears == 0 && deltaDays == 1) {
+            user.loginStreak++ // Increase the login streak
+        } else {
+            user.loginStreak = 0 // Otherwise reset it
+        }
+
+        user.lastLogin = currentLogin // Set the login date
+        user.save() // Save
+        
+        done(null, user.uid) // Serialize
     })
 
     passport.deserializeUser((uid, done) => {
-        done(null, Users.findOne({uid: uid}))
+        done(null, Users.findOne({uid: uid})) // Deserialize 
     })
 }

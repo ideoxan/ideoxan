800f43043761d004fc0da9b3f7dc18b35fcf6c0f
Skyclo <hello.skyclo@gmail.com>
2021-06-17 17:54:26 -0400

    🔥 wip: jk ya yeet

--- static/js/editor/editor.js
+++ static/js/editor/editor.js
@@ -1,6 +1,4 @@
-import BalkanCore from "./Balkan/BalkanCore.js";
-
-const Editor = {}
+window.Editor = {}
 
 function init () {
     if ( document.querySelector( 'html' ).classList.contains( 'dark' ) ) {
@@ -13,25 +11,15 @@ function init () {
 }
 
 async function ready () {
-    Editor.BalkanRender = new BalkanCore.Render('#balkan')
-    Editor.BalkanKeyManager = new BalkanCore.KeyManager('en_us')
-    Editor.BalkanSession = new BalkanCore.Session()
-    Editor.BalkanSession.registerFile(new BalkanCore.File('app.js', 'javascript'))
-    Editor.BalkanSession.files['app.js'].setValue('let str = "Hello, World!"')
-    Editor.BalkanSession.files['app.js'].insertLine(1, 'console.log(str)')
-    Editor.BalkanSession.files['app.js'].insertLine(2, '')
-    
-    console.log('Balkan Ready')
+
 }
 
 async function update () {
-    Editor.BalkanRender.drawUpdate(Editor.BalkanSession)
+
 }
 
 async function strokeUpdate (keyboardEvent) {
-    console.log(keyboardEvent.keyCode)
-    Editor.BalkanKeyManager.handleKeyEvent(Editor.BalkanSession, keyboardEvent)
-    console.log(Editor.BalkanSession.position)
+
 }
 
 
@@ -47,7 +35,6 @@ window.addEventListener('load', async () => {
         await update()
     }, Editor.CONSTANTS.fixedUpdateInterval)
     document.addEventListener('keydown', async (event) => {
-        await strokeUpdate(event)
         await update()
     })
 })
